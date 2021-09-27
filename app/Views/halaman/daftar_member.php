87885d4e343263bea437632b4e5bb6051cb05b49
12200571viktojuniyadi <kingn9809@gmail.com>
2021-09-27 15:38:55 +0700

    Tugas pertemuan 2

---
+++ app/Views/halaman/daftar_member.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Member</title>
+</head>
+<body>
+    <h1>Hai... Kamu mau jadi member ?</h1>
+    <p>isikan data diri kamu dulu ya agar kami kenal dengan member kami</p>
+    
+</body>
+</html>
---
+++ app/Views/halaman/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda Login</title>
+</head>
+<body>
+    <h1>Selamat datang di sistem informasi Perpustakaan</h1>
+    <p>Ini adalah layanan perpustakaan digital masa kini
+        <a href="<?=base_url('/daftar-member')?>">Klik daftar</a>
+    </p>
+    
+</body>
+</html>
